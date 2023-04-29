3a8e0d040e4ac5adb712e88277fc3c665f063e94
Manomay Kagalkar <manomay111@gmail.com>
2023-04-29 22:57:39 +0530

    minor fix
    logo issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import tuskLogo from '../../public/tusk.png'
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Header() {
     return(
         <aside className="flex flex-col gap-5 md:flex-row justify-between self-center items-center h-1/6 w-9/12">
-            <Image className="flex w-[200px]" src={tuskLogo} alt='tusk logo' priority={true}></Image>
+            <Image src={'/tusk.png'} alt='tusk logo' width={200} height={200} priority={true}></Image>
             <nav className='flex font-bold flex-row gap-5 md:gap-10 md:text-2xl text-lg'>
                 <Link className="hover:text-tusklet hover:animate-pulse" href={'/home'}>/home</Link>
                 <Link className="hover:text-tusklet hover:animate-pulse" href={'/about'}>/about</Link>
