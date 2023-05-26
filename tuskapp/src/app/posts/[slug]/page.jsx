260dabb5a33a5ee31e5849640ce8a947269800f6
Manomay Kagalkar <manomay111@gmail.com>
2023-05-26 12:58:40 +0530

    testing vercel deployment

--- tuskapp/src/app/posts/[slug]/page.jsx
+++ tuskapp/src/app/posts/[slug]/page.jsx
@@ -7,7 +7,7 @@ import getPost from '@/components/getPost'
 
 export async function generateMetadata() {
     return {
-        title: 'TUSK Blog Post: TITLE',
+        title: 'TUSK Article',
         author: 'AUTHOR',
         description: 'DESCRIPTION',
     }
